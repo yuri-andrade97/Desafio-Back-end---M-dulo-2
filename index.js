efc1cca4fd143189b96e40cd5bc17fd313d08f90
yuri-andrade97 <dev.yuri97@gmail.com>
2021-06-12 19:16:46 -0300

    funçao listar produtos que contenha no estoque, finalizada

--- index.js
+++ index.js
@@ -9,7 +9,21 @@ const data = require('./data/data.json')
 app.use(express.json());
 
 app.get('/produtos', (req, res) => {
+    console.log(data.produtos.length)
     res.json(data.produtos);
+});
+
+app.get('/produtos/:estoque', (req, res) => {
+    const produtos = data.produtos;
+    const temNoEstoque = [];
+
+    for (const produto of produtos) {
+        if(produto.estoque > 0) {
+            temNoEstoque.push(produto);            
+        }
+    }
+    console.log(temNoEstoque.length)
+    res.json(temNoEstoque)
 });
 
 app.listen(8000);
