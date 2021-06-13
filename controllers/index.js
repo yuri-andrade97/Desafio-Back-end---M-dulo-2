e6dc54c27c0f468442eee2af473fa925d14b66e9
yuri-andrade97 <dev.yuri97@gmail.com>
2021-06-13 10:58:59 -0300

    função GET/produtos, implementada

--- controllers/index.js
+++ controllers/index.js
@@ -46,7 +46,6 @@ function listarProdutos(req, res) {
                 }
             }
         });
-        console.log("Filtrar produtos por categoria")
         return res.json(produtosFiltradoPorCategoria);
     }
 
--- routes.js
+++ routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { listarProdutos } = require('./controllers/index');
 
-
 const router = express();
 
 router.get('/produtos', listarProdutos)
