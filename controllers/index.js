9c587fc93a2d4d4406e43a6715f90b8b303b45fc
yuri-andrade97 <dev.yuri97@gmail.com>
2021-06-15 22:21:15 -0300

    desafio concluido!

--- controllers/index.js
+++ controllers/index.js
@@ -60,9 +60,8 @@ function listarProdutos(req, res) {
 
 async function listarProdutosNoCarrinho(req, res) {
     let data = await lerArquivo()
-    res.json(data)
+    return res.json(data)
 
-    // depois tenho que verificar caso os dados estejam com array VAZIO
 }
 
 async function adicionarProdutoNoCarinho(req, res) {
@@ -94,38 +93,139 @@ async function adicionarProdutoNoCarinho(req, res) {
         }
         await escreverNoArquivo(data);
         
-        res.json(data)
+        return res.json(data)
     }
+    return res.status(400).json({erro: 'Quantidade maior do que possui no estoque'})
 }
 
 async function alterarCarrinho(req, res) {
     let carrinho = await lerArquivo()
+    
     const { idProduto } = req.params;
     const quantidade = req.body.quantidade;
+    const { produtos } = require('../data/data.json');
     
+    // verificando se tem o produto no carrinho (comparação feita através do ID)
     const localizadorDeProdutos = await carrinho.produtos.find(produto => produto.id === Number(idProduto));
 
     if (!localizadorDeProdutos) {
-        res.json({erro: `Produto com id ${idProduto} não encontrado no carrinho!`})
+        return res.json({erro: `Produto com id ${idProduto} não encontrado no carrinho!`})
     }
 
-    if (quantidade < 0) {
-        if (quantidade > carrinho.quantidade) {
-            res.json({erro: 'a quantidade solicitada é maior doque a no estoque!'})
-        } else {
-            const indice = carrinho.produtos.findIndex(produto => produto.id === Number(idProduto));
+    const produtoEstoque = produtos.find(produto => produto.id === Number(idProduto));
+         
+    const indice = carrinho.produtos.findIndex(produto => produto.id === Number(idProduto));
 
-            carrinho.produtos[indice].quantidade -= quantidade
-            return carrinho
+
+    if ( (quantidade + carrinho.produtos.quantidade) > produtos.estoque ) {
+        return res.json({erro: 'Quantidade maior, doque possue no estoque'})   
+    }
+    carrinho.produtos[indice].quantidade += quantidade;
+        
+    if (carrinho.produtos[indice].quantidade <= 0) {
+        return res.json({erro: "Indice invalido"})
+    }
+    if (carrinho.produtos[indice].quantidade > produtoEstoque.estoque) {
+        return res.json({erro: 'a quantidade solicitada é maior doque a no estoque!'});
+    } 
+    carrinho.subtotal = calcularSubtotal(carrinho.produtos)
+    await escreverNoArquivo(carrinho);
+
+    res.json(carrinho)
+}
+
+async function deletarProduto(req, res) {
+    let carrinho = await lerArquivo()
+    const id = req.params.idProduto;
+
+    const indice = carrinho.produtos.findIndex(produto => produto.id === Number(id));
+
+    if (indice === -1) {
+        return res.json({erro: `Id ${id} não encontrado no carrinho!`});
+    }
+    carrinho.produtos.splice(indice, 1)
+    await escreverNoArquivo(carrinho)
+
+    res.json(carrinho)
+    // aqui ainda precisa após retirar o  produto, limpar o subtotal e etc...
+}
+
+async function deletarTodosOsProdutosDoCarrinho(req, res) {
+    let carrinho = await lerArquivo();
+
+    carrinho = {produtos:[]}
+    await escreverNoArquivo(carrinho)
+    res.json(carrinho)
+}
+
+async function finalizarCompra(req, res) {
+    let carrinho = await lerArquivo()
+    const data = require('../data/data.json');
+
+
+    if (carrinho.produtos.length === 0) {
+        return res.json({erro: 'Não foi possível finalizar a compra, pois o carrinho está VAZIO!'})
+    }
+
+    for (const item of carrinho.produtos) {
+        const localizarProdutoNoBanco = data.produtos.find(produto => produto.id === item.id);
+        if (item.quantidade > localizarProdutoNoBanco.estoque) {
+           return res.json('erro!')
         }
     }
+
+    const erro = validardorDeDados(req.body)
+    
+    if(erro.length > 0) {
+        res.status(400).json(erro)
+    }
+
+    res.json(carrinho)
+    carrinho = {produtos:[]}
+    await escreverNoArquivo(carrinho)
+
+    return
+
+}
+
+function validardorDeDados(dados) {
+    const { type, country, name, documents } = dados.customer;
+    const erro = [];
+    let msgErro = "";
+
+    if (country.length !== 2 ) {
+        msgErro = 'O campo country deve possuir somente 2 dígitos.Exemplo, se você mora no Brasil, br';
+        erro.push(msgErro);
+    }
+
+    if (type !== 'individual') {
+        msgErro = 'Este e-commerce só atende pessoas físicas, o type precisa ser INDIVIDUAL';
+        erro.push(msgErro)
+    }
+
+    const nameFormatado = name.split('').find(item => item === " ");
+    
+    if (!nameFormatado) {
+        msgErro = 'O campo name, deve possuir o nome e o sobrenome'
+        erro.push(msgErro);
+    }
+    
+    if (documents[0].type !== 'cpf') {
+        msgErro = 'O campo type, deve possuir ser sempre o CPF'
+        erro.push(msgErro);
+    }
+
+    if (documents[0].number.length !== 11) {
+        msgErro = 'O campo CPF, deve possuir 11 dígitos!'
+        erro.push(msgErro);
+    }
+
+    return erro;
 }
 
 /*  FUNÇÕES AUXILIAR PARA A ROTA POST/CARRINHOS/PRODUTOS */
 function adicionarProdutos(produto, carrinho, quantidade) {
     delete produto.estoque
-
-    console.log(carrinho)
    
     const indice = carrinho.findIndex(item => item.id === produto.id);
    
@@ -172,5 +272,8 @@ module.exports = {
     listarProdutos,
     adicionarProdutoNoCarinho,
     listarProdutosNoCarrinho,
-    alterarCarrinho
+    alterarCarrinho,
+    deletarProduto,
+    deletarTodosOsProdutosDoCarrinho,
+    finalizarCompra
 }
--- routes.js
+++ routes.js
@@ -1,12 +1,18 @@
 const express = require('express');
-const { listarProdutos, adicionarProdutoNoCarinho, listarProdutosNoCarrinho, alterarCarrinho } = require('./controllers/index');
+const { 
+    listarProdutos, adicionarProdutoNoCarinho, listarProdutosNoCarrinho, alterarCarrinho, deletarProduto, deletarTodosOsProdutosDoCarrinho, finalizarCompra 
+} = require('./controllers/index');
 
 const router = express();
 
 router.get('/produtos', listarProdutos);
 
-router.get('/carrinho', listarProdutosNoCarrinho)
+router.get('/carrinho', listarProdutosNoCarrinho);
 router.post('/carrinho/produtos', adicionarProdutoNoCarinho);
-router.patch('/carrinho/produtos/:idProduto', alterarCarrinho)
+router.patch('/carrinho/produtos/:idProduto', alterarCarrinho);
+router.delete('/carrinho/produtos/:idProduto', deletarProduto);
+router.delete('/carrinho', deletarTodosOsProdutosDoCarrinho);
+
+router.post('/carrinho/finalizar-compra', finalizarCompra)
 
 module.exports = router;
